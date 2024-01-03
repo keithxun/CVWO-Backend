98d21bba983d47a9c87cd963b561f48b215bffd6
keith <keithonghx@gmail.com>
2024-01-03 10:18:35 +0800

    Nil

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/current_user', to: 'current_user#index'
+  # get '/current_user', to: 'current_user#index'
   resources :posts do
     resources :comments
   end
